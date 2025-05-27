009afcd5bf7ee07358e0a4265c207facbde3c69c
Oliver Forral <intrepion@gmail.com>
2025-05-26 21:59:48 -0600

    Displaying dynamic data

--- ch04/my-app/src/app/app.component.ts
+++ ch04/my-app/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CopyrightDirective } from './copyright.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [
+    RouterOutlet,
+    ProductListComponent,
+    CopyrightDirective,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-app';
 }
