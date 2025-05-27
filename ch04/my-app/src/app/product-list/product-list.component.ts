c6bc216a9394d900a7e55fcafc15069d63cbc21a
Oliver Forral <intrepion@gmail.com>
2025-05-26 18:45:19 -0600

    cp -R ch03 ch04

---
+++ ch04/my-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, viewChild } from '@angular/core';
+import { Product } from '../product';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [ProductDetailComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+
+export class ProductListComponent implements AfterViewInit {
+  productDetail = viewChild(ProductDetailComponent);
+
+  products: Product[] = [
+    { id: 1, title: 'Keyboard' },
+    { id: 2, title: 'Microphone' },
+    { id: 3, title: 'Web camera' },
+    { id: 4, title: 'Tablet' }
+  ];
+
+  selectedProduct: Product | undefined = this.products[0];
+
+  ngAfterViewInit(): void {
+    console.log(this.productDetail()!.product());
+  }
+
+  onAdded(product: Product) {
+    alert(`${product.title} added to the cart!`);
+  }
+}
