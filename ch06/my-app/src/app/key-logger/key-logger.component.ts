f4cd8887c2d75e655b25aedad0bd393e7fe13619
Oliver Forral <intrepion@gmail.com>
2025-05-30 15:58:28 -0600

    ng generate component key-logger

---
+++ ch06/my-app/src/app/key-logger/key-logger.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-logger',
+  imports: [],
+  templateUrl: './key-logger.component.html',
+  styleUrl: './key-logger.component.css'
+})
+export class KeyLoggerComponent {
+
+}
