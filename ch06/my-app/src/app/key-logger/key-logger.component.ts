d5a1b2459cfc2dcc33a0a42c692f0049c812c883
Oliver Forral <intrepion@gmail.com>
2025-05-30 21:47:06 -0600

    Reactive programming in Angular

--- ch06/my-app/src/app/key-logger/key-logger.component.ts
+++ ch06/my-app/src/app/key-logger/key-logger.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './key-logger.component.html',
   styleUrl: './key-logger.component.css'
 })
-export class KeyLoggerComponent {
 
+export class KeyLoggerComponent implements OnInit {
+  input = viewChild<ElementRef>('keyContainer');
+
+  keys = '';
+
+  ngOnInit(): void {
+    const logger$ = fromEvent<KeyboardEvent>(this.input()!.
+    nativeElement, 'keyup');
+    logger$.subscribe(evt => this.keys += evt.key);
+  }
 }
