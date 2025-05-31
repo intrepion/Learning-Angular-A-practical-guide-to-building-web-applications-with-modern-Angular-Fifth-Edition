05d1ba448cdb597478c8765f484e6f95900eb2dd
Oliver Forral <intrepion@gmail.com>
2025-05-31 10:55:58 -0600

    Transforming observables

--- ch06/my-app/src/app/key-logger/key-logger.component.ts
+++ ch06/my-app/src/app/key-logger/key-logger.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { Component, ElementRef, input, OnInit, viewChild } from '@angular/core';
+import { filter, fromEvent, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -13,9 +13,19 @@ export class KeyLoggerComponent implements OnInit {
 
   keys = '';
 
+  numeric = input(false);
+
   ngOnInit(): void {
-    const logger$ = fromEvent<KeyboardEvent>(this.input()!.
-    nativeElement, 'keyup');
-    logger$.subscribe(evt => this.keys += evt.key);
+    const logger$ = fromEvent<KeyboardEvent>(this.input()!.nativeElement, 'keyup');
+    logger$.pipe(
+      map(evt => evt.key.charCodeAt(0)),
+      filter(code => {
+        if (this.numeric()) {
+          return (code > 31 && (code < 48 || code > 57)) === false;
+        }
+        return true;
+      }),
+      tap(digit => this.keys += String.fromCharCode(digit))
+    ).subscribe();
   }
 }
