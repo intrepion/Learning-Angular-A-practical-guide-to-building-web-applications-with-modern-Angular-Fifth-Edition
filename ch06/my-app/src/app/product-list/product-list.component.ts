bb33142f3d2664bdbb53c6627647ca792818588d
Oliver Forral <intrepion@gmail.com>
2025-05-31 14:53:28 -0600

    Destroying a component

--- ch06/my-app/src/app/product-list/product-list.component.ts
+++ ch06/my-app/src/app/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, Component, inject, OnInit, viewChild } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit, viewChild } from '@angular/core';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../sort.pipe';
 import { ProductsService } from '../products.service';
 import { FavoritesComponent } from '../favorites/favorites.component';
 import { ProductViewComponent } from '../product-view/product-view.component';
+import { Subscription } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-list',
@@ -16,17 +18,23 @@ import { ProductViewComponent } from '../product-view/product-view.component';
   ],
 })
 
-export class ProductListComponent implements AfterViewInit, OnInit {
-  private productService = inject(ProductsService);
+export class ProductListComponent implements AfterViewInit, OnDestroy, OnInit {
+  private destroyRef = inject(DestroyRef);
 
   productDetail = viewChild(ProductDetailComponent);
 
   products: Product[] = [];
 
+  private productService = inject(ProductsService);
+
+  private productsSub: Subscription | undefined;
+
   selectedProduct: Product | undefined = this.products[0];
 
   private getProducts() {
-    this.productService.getProducts().subscribe(products => {
+    this.productsSub = this.productService.getProducts().pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(products => {
       this.products = products;
     });
   }
@@ -41,5 +49,9 @@ export class ProductListComponent implements AfterViewInit, OnInit {
 
   onAdded(product: Product) {
     alert(`${product.title} added to the cart!`);
+  }
+
+  ngOnDestroy(): void {
+    this.productsSub?.unsubscribe();
   }
 }
