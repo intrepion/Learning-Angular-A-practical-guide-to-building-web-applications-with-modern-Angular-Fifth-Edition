b0af83956e24f10653a5a4d9e61928570414bcb5
Oliver Forral <intrepion@gmail.com>
2025-06-02 22:07:05 -0600

    Authenticating with a backend API

--- ch08/my-app/src/app/auth.service.ts
+++ ch08/my-app/src/app/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { APP_SETTINGS } from './app.settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private accessToken = signal('');
+  private authUrl = inject(APP_SETTINGS).apiUrl + '/auth';
+  isLoggedIn = computed(() => this.accessToken() !== '');
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<string> {
+    return this.http.post<string>(this.authUrl + '/login', {
+      username, password
+    }).pipe(tap(token => this.accessToken.set(token)));
+  }
+
+  logout() {
+    this.accessToken.set('');
+  }
 }
