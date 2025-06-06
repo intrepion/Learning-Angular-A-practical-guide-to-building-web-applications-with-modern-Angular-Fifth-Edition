d2ebb9d81b2f6b3da710ae5c71be32439f4c05eb
Oliver Forral <intrepion@gmail.com>
2025-06-06 17:28:20 -0600

    Preventing navigation away from a route

--- ch09/my-app/src/app/app.routes.ts
+++ ch09/my-app/src/app/app.routes.ts
@@ -4,16 +4,34 @@ import { CartComponent } from './cart/cart.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { checkoutGuard } from './checkout.guard';
 
 export const routes: Routes = [
-  { path: 'products/new', component: ProductCreateComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products', component: ProductListComponent },
   {
-    path: 'cart',
-    component: CartComponent,
-    canActivate: [authGuard],
+    component: ProductCreateComponent,
+    path: 'products/new',
   },
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: '**', redirectTo: 'products' },
+  {
+    component: ProductDetailComponent,
+    path: 'products/:id',
+  },
+  {
+    component: ProductListComponent,
+    path: 'products',
+  },
+  {
+    canActivate: [authGuard],
+    canDeactivate: [checkoutGuard],
+    component: CartComponent,
+    path: 'cart',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
 ];
--- ch09/my-app/src/app/checkout.guard.ts
+++ ch09/my-app/src/app/checkout.guard.ts
@@ -1,5 +1,10 @@
 import { CanDeactivateFn } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
-export const checkoutGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
+export const checkoutGuard: CanDeactivateFn<CartComponent> = () => {
+  const confirmation = confirm(
+    'You have pending items in your cart. Do you want to continue?'
+  );
+
+  return confirmation;
 };
