632739ddf8d263e9b29799d5a230e547b5918e23
Oliver Forral <intrepion@gmail.com>
2025-06-06 16:59:53 -0600

    Controlling route access

--- ch09/my-app/src/app/app.routes.ts
+++ ch09/my-app/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 import { CartComponent } from './cart/cart.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -8,7 +9,11 @@ export const routes: Routes = [
   { path: 'products/new', component: ProductCreateComponent },
   { path: 'products/:id', component: ProductDetailComponent },
   { path: 'products', component: ProductListComponent },
-  { path: 'cart', component: CartComponent },
+  {
+    path: 'cart',
+    component: CartComponent,
+    canActivate: [authGuard],
+  },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: '**', redirectTo: 'products' },
 ];
