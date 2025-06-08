b0af5c0c379b6a1af6b3a253a99a5dfb36dba407
Oliver Forral <intrepion@gmail.com>
2025-06-08 03:26:52 -0700

    Interacting with reactive forms

--- ch10/my-app/src/app/product-create/product-create.component.ts
+++ ch10/my-app/src/app/product-create/product-create.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from '../products.service';
 
 @Component({
-  selector: 'app-product-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   providers: [
     { provide: ProductsService, useClass: ProductsService },
   ],
+  selector: 'app-product-create',
+  styleUrl: './product-create.component.css',
   templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
 })
 
 export class ProductCreateComponent {
+  productForm = new FormGroup({
+    title: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, { nonNullable: true }),
+    category: new FormControl('', { nonNullable: true })
+  });
+
   constructor(private productsService: ProductsService, private router: Router) {}
 
-  createProduct(title: string, price: string, category: string) {
-    this.productsService.addProduct({
-      title,
-      price: Number(price),
-      category
-    }).subscribe(() => this.router.navigate(['/products']));
+  createProduct() {
+    this.productsService.addProduct(this.productForm.value).subscribe(() => {
+      this.router.navigate(['/products']);
+    });
   }
 }
