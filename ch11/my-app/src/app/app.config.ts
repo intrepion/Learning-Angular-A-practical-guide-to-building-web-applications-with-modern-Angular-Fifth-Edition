f512278874079d61c11a631c4ad6dadb0f5d9764
Oliver Forral <intrepion@gmail.com>
2025-06-10 14:01:37 -0700

    Creating a global error handler

--- ch11/my-app/src/app/app.config.ts
+++ ch11/my-app/src/app/app.config.ts
@@ -1,17 +1,19 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { APP_SETTINGS, appSettings } from './app.settings';
+import { AppErrorHandler } from './app-error-handler';
 import { authInterceptor } from './auth.interceptor';
 import { ProductsService } from './products.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_SETTINGS, useValue: appSettings },
+    { provide: ErrorHandler, useClass: AppErrorHandler },
+    ProductsService,
     provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    { provide: APP_SETTINGS, useValue: appSettings },
-    ProductsService,
   ]
 };
