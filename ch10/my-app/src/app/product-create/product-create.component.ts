cddbad682f4e21b7a671a5d87c48b9f2a5b9a11f
Oliver Forral <intrepion@gmail.com>
2025-06-09 22:49:57 -0700

    Validation in reactive forms

--- ch10/my-app/src/app/product-create/product-create.component.ts
+++ ch10/my-app/src/app/product-create/product-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from '../products.service';
 
@@ -14,11 +14,17 @@ import { ProductsService } from '../products.service';
 })
 
 export class ProductCreateComponent implements OnInit{
-  productForm: FormGroup<{
-    title: FormControl<string>,
-    price: FormControl<number | undefined>,
-    category: FormControl<string>
-  }> | undefined;
+  productForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required
+    }),
+    price: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)]
+    }),
+    category: new FormControl('', { nonNullable: true })
+  });
 
   constructor(
     private productsService: ProductsService,
