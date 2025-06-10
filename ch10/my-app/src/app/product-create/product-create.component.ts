3381ae7fb3a40698dcc967553d04a0c77e5b8cf4
Oliver Forral <intrepion@gmail.com>
2025-06-10 00:12:04 -0700

    Reacting to state changes

--- ch10/my-app/src/app/product-create/product-create.component.ts
+++ ch10/my-app/src/app/product-create/product-create.component.ts
@@ -54,6 +54,8 @@ export class ProductCreateComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.buildForm();
+    this.productForm.controls.category.valueChanges.subscribe(() => {
+      this.productForm.controls.price.reset();
+    });
   }
 }
