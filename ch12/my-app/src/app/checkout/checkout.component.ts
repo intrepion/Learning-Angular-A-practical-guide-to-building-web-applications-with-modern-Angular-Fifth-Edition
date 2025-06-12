5c836ed82da40e976978abc101159ac0a1c20832
Oliver Forral <intrepion@gmail.com>
2025-06-12 11:18:28 -0700

    Creating a confirmation dialog

--- ch12/my-app/src/app/checkout/checkout.component.ts
+++ ch12/my-app/src/app/checkout/checkout.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
+  imports: [MatButton, MatDialogModule],
   selector: 'app-checkout',
-  imports: [],
+  styleUrl: './checkout.component.css',
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
 })
+
 export class CheckoutComponent {
 
 }
