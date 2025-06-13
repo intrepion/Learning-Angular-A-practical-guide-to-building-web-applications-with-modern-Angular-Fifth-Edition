062cda6f8035e5ba37daf771dbe7b637940385ea
Oliver Forral <intrepion@gmail.com>
2025-06-12 23:58:33 -0700

    ng generate component async

---
+++ ch13/my-app/src/app/async/async.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-async',
+  imports: [],
+  templateUrl: './async.component.html',
+  styleUrl: './async.component.css'
+})
+export class AsyncComponent {
+
+}
