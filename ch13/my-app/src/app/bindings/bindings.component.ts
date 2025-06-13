bcf326386143d0787e5828b9f62b2b29c057737f
Oliver Forral <intrepion@gmail.com>
2025-06-13 00:21:11 -0700

    ng generate component bindings

---
+++ ch13/my-app/src/app/bindings/bindings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bindings',
+  imports: [],
+  templateUrl: './bindings.component.html',
+  styleUrl: './bindings.component.css'
+})
+export class BindingsComponent {
+
+}
