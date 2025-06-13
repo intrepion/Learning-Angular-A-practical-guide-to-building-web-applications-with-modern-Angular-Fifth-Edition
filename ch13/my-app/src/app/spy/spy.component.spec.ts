d26033b20243f8decf113d0ef85a8fd0f60949c7
Oliver Forral <intrepion@gmail.com>
2025-06-12 23:58:01 -0700

    Spying on the dependency method

--- ch13/my-app/src/app/spy/spy.component.spec.ts
+++ ch13/my-app/src/app/spy/spy.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpyComponent } from './spy.component';
+import { Title } from '@angular/platform-browser';
 
 describe('SpyComponent', () => {
   let component: SpyComponent;
@@ -20,4 +21,24 @@ describe('SpyComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});
+
+it('should get the title', async () => {
+  const titleSpy = jasmine.createSpyObj('Title', [
+    'getTitle', 'setTitle'
+  ]);
+  titleSpy.getTitle.and.returnValue('My title');
+  
+  await TestBed.configureTestingModule({
+    imports: [SpyComponent],
+    providers: [
+    { provide: Title, useValue: titleSpy }
+    ]
+  })
+  .compileComponents();
+  
+  const fixture = TestBed.createComponent(SpyComponent);
+  fixture.detectChanges();
+  
+  expect(fixture.nativeElement.textContent).toContain('My title');
 });
--- ch13/my-app/src/app/spy/spy.component.ts
+++ ch13/my-app/src/app/spy/spy.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-spy',
   imports: [],
+  selector: 'app-spy',
+  styleUrl: './spy.component.css',
   templateUrl: './spy.component.html',
-  styleUrl: './spy.component.css'
 })
-export class SpyComponent {
 
+export class SpyComponent {
+  caption = '';
+
+  constructor(private title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('My Angular app');
+    this.caption = this.title.getTitle();
+  }
 }
